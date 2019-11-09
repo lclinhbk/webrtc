07557cb1c17409f2efe152538a9175ef6659a974
lclinhbk <49783030+lclinhbk@users.noreply.github.com>
2019-11-09 16:09:00 +0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -28,7 +28,7 @@ socket.on('DANH_SACH_ONLINE', arrUserInfo => {
         const { ten, peerId, stt, firstCamId, firstCamTen } = user;
         $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
         console.log(firstCamId+"nguoi dung cu");
-        if (firstCamId && firstCamId != peerId  ) {
+        if (firstCamId && firstCamId != peerId && $("#" + firstCamId).length == 0 ) {
             $('#ulUser').append(`<li id="${firstCamId}" class="tu arrUserInfo">${firstCamTen}</li>`);
             $("#"+firstCamId).click();
         }
@@ -40,7 +40,7 @@ socket.on('DANH_SACH_ONLINE', arrUserInfo => {
         console.log(peerId+"nguoi dung moi");
         console.log(stt);
         
-        if (firstCamId && firstCamId != peerId ) {
+        if (firstCamId && firstCamId != peerId && $("#" + firstCamId).length == 0) {
             $('#ulUser').append(`<li id="${firstCamId}" class="tu CO_NGUOI_DUNG_MOI">${firstCamTen}</li>`);
             $("#"+firstCamId).click();
         }
