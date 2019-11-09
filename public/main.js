779258910be30a0bc55e8b19a5699bc449033dc7
lclinhbk <49783030+lclinhbk@users.noreply.github.com>
2019-11-09 11:30:30 +0700

    ab

--- public/main.js
+++ public/main.js
@@ -27,14 +27,14 @@ socket.on('DANH_SACH_ONLINE', arrUserInfo => {
         const { ten, peerId } = user;
         $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
         console.log(peerId);
-        $("#"+peerID).click();
+        $("#"+peerId).click();
     });
 
     socket.on('CO_NGUOI_DUNG_MOI', user => {
         const { ten, peerId } = user;
         $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
         console.log(peerId+"nguoi dung moi");
-        $("#"+peerID).click();
+        $("#"+peerId).click();
     });
 
     socket.on('AI_DO_NGAT_KET_NOI', peerId => {
@@ -127,6 +127,6 @@ $('#ulUser').on('click', 'li', function() {
 $( document ).ready(function() {
   // Handler for .ready() called.
     $("#txtUsername").val("linh");
-    $("btnSignUp").click();
+    $("#btnSignUp").click();
     console.log(234);
 });
